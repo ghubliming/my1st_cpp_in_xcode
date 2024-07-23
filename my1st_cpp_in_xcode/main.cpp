aaa2028f7f17c308df3049ccdf2f5a97c5e7653c
LmLiu <llmmail2021@icloud.com>
2024-07-23 10:43:14 +0200

    Initial Commit

---
+++ my1st_cpp_in_xcode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  my1st_cpp_in_xcode
+//
+//  Created by liu on 19.07.24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
